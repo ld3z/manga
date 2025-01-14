300d54ab76d3189d422c9d33d889db00f09eb93c
ld3zian <42660576+ld3z@users.noreply.github.com>
2025-01-13 18:17:47 -0700

    add astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
+import betterImageService from 'astro-better-image-service';
+
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  integrations: [betterImageService()],
 });
